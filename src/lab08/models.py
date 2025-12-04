758729a4bbcac681d18f8dad6d69e49de6ccf8cb
Ronan181 <ronan9870@gmail.com>
2025-12-04 20:24:50 +0300

    R

--- src/lab08/models.py
+++ src/lab08/models.py
@@ -5,23 +5,21 @@ from datetime import datetime, date
 @dataclass
 class Student:
     fio: str
-    birthdate: str  
+    birthdate: str
     group: str
-    gpa: float       
+    gpa: float
 
-    # Валидация
     def __post_init__(self):
-        # корректный формат даты
         try:
             datetime.strptime(self.birthdate, "%Y-%m-%d")
         except ValueError:
             raise ValueError("birthdate must be in format YYYY-MM-DD")
-        # gpa 0–5
+        self.gpa = float(self.gpa)
         if not (0 <= self.gpa <= 5):
-            raise ValueError()
+            raise ValueError(f"GPA должен быть от 0 до 5, получено {self.gpa}")
 
     # Возраст
-    def age(self) -> int:
+    def age(self):
         b = datetime.strptime(self.birthdate, "%Y-%m-%d").date()
         today = date.today()
         years = today.year - b.year
@@ -30,7 +28,7 @@ class Student:
         return years
 
     # Сериализация
-    def to_dict(self) -> dict:
+    def to_dict(self):
         return {
             "fio": self.fio,
             "birthdate": self.birthdate,
@@ -40,16 +38,16 @@ class Student:
 
     # Десериализация
     @classmethod
-    def from_dict(cls, d: dict):
+    def from_dict(cls, d):
         return cls(
             fio=d["fio"],
             birthdate=d["birthdate"],
             group=d["group"],
-            gpa=d["gpa"],
+            gpa=float(d["gpa"]),  
         )
-    
+
     def __str__(self):
-        return f" Студент: {self.fio}, группа {self.group}, GPA {self.gpa}, возраст {self.age()}"
+        return f"Студент: {self.fio}, группа {self.group}, GPA {self.gpa}, возраст {self.age()}"
 
 s = Student(fio="Иванов Петр", birthdate="2007-10-19", group="BIVT-25-8", gpa=4.8)
 print(s)
